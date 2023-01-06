eaf684b2fda6da30102f160942ef7e1243bde845
JasonTejadaRevature <jason263@revature.net>
2023-01-06 04:35:20 -0800

    Set up the angular project for login implementation

---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  /** 
+    * TODO Send a login request to ERS api
+    * - Create the login form
+    * - Get input from login form to be sent to api
+    * - Make a login request using the frontend
+    */
+}
